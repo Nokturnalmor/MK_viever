81e68650e9421250de8f5506631ea749c7eae320
Nokturnalmor <76943309+Nokturnalmor@users.noreply.github.com>
2021-02-02 20:04:32 +0300

    Добавлен цвет выбора детали просмотр наряда опечатки оформление для нарядов

--- Viever.py
+++ Viever.py
@@ -47,6 +47,9 @@ class mywindow(QtWidgets.QMainWindow):
 
         tabl_mk = self.ui.table_mk_view
         tabl_mk.doubleClicked.connect(self.dblclk_mk)
+        tabl_mk.setSelectionBehavior(1)
+        tabl_mk.setSelectionMode(1)
+        F.ust_cvet_videl_tab(tabl_mk)
 
         lineEdit_mk = self.ui.lineEdit_mk
         lineEdit_mk.textEdited.connect(self.poisk_mk)
@@ -71,6 +74,28 @@ class mywindow(QtWidgets.QMainWindow):
             self.vigruz_tehkart(r,k)
         if k >10 and (k-12)%4==0:
             self.vigruz_tara(r,k)
+        if k >10 and (k-13)%4==0:
+            self.vigruz_narad(r,k)
+
+    def vigruz_narad(self, r, k):
+        tabl_mk = self.ui.table_mk_view
+        if tabl_mk.item(r, k).text() == "":
+            return
+        tabl_sp_mk = self.ui.table_mk
+        id = tabl_mk.item(r, 6).text().strip()
+        nom_mk = tabl_sp_mk.item(tabl_sp_mk.currentRow(), 0).text()
+        arr = tabl_mk.item(r, k).text().split('\n')
+        sp_nar = F.otkr_f(F.tcfg('Naryad'), separ='|')
+        s= ''
+        for i in range(len(arr)):
+            arr2 = arr[i].split(' ')
+            nom_nar = arr2[0]
+            for j in range(1,len(sp_nar)):
+                if sp_nar[j][0] == nom_nar:
+                    s += nom_nar + ' выдан ' + sp_nar[j][2] + ' ' + sp_nar[j][7] + ' на ' + sp_nar[j][9] + \
+                         ' для :' + '\n' + '    ' + sp_nar[j][17] + ','+ sp_nar[j][18] + '\n' + '\n'
+        showDialog(self, s)
+        return
 
     def vigruz_tara(self, r, k):
         tabl_mk = self.ui.table_mk_view
@@ -158,7 +183,7 @@ class mywindow(QtWidgets.QMainWindow):
         tabl_sp_mk = self.ui.table_mk
         nom = tabl_sp_mk.item(tabl_sp_mk.currentRow(),0).text()
         if F.nalich_file(F.scfg('mk_data') + os.sep + nom + '.txt') == False:
-            showDialog(self, 'Не обнаржун файл')
+            showDialog(self, 'Не обнаржен файл')
             return
         sp = F.otkr_f(F.scfg('mk_data') + os.sep + nom + '.txt',False,'|')
         if sp == []:
@@ -179,9 +204,12 @@ class mywindow(QtWidgets.QMainWindow):
                 break
         return int(n/4)
 
+
     def oform_mk(self,sp):
         shag = 15
         tabl_mk = self.ui.table_mk_view
+        tabl_sp_mk = self.ui.table_mk
+
         maxs = set()
         for i in range(1,len(sp)):
             maxs.add(self.uroven(sp[i][0]))
@@ -202,15 +230,100 @@ class mywindow(QtWidgets.QMainWindow):
                         F.dob_color_wtab(tabl_mk, i - 1, j, 0, 127, 0)
                     else:
                         F.dob_color_wtab(tabl_mk, i - 1, j, 37, 17, 0)
+                if tabl_mk.item(i - 1, j+1).text() != '':
+                    arr = tabl_mk.item(i - 1, j+1).text().strip().split('\n')
+                    flag = 0
+                    set_sost = set()
+                    for k in range(len(arr)):
+                        arr2 = arr[k].split(' ')
+                        set_sost.add(arr2[1])
+                    if len(set_sost) == 1 and 'Завершен' in set_sost:
+                        flag = 2
+                    elif len(set_sost) == 1 and 'Выдан' in set_sost:
+                        flag = 0
+                    else:
+                        flag = 1
+
+                    if flag == 1:# оранж
+                        F.dob_color_wtab(tabl_mk, i - 1, j+1, 37, 17, 0)
+                    if flag == 0:
+                        if tabl_mk.currentRow() == -1:
+                            showDialog(self, 'Не выбрана дсе')
+                            return
+                        if tabl_sp_mk.currentRow() == -1:
+                            showDialog(self, 'Не выбрана мк')
+                            return
+                    nom_mk = tabl_sp_mk.item(tabl_sp_mk.currentRow(), 0).text()
+                    id_dse = tabl_mk.item(i - 1, 6).text()
+                    arr_op = tabl_mk.item(i - 1, j - 1).text()
+                    arr_op2 = arr_op.split('Операции:\n')
+                    obr = arr_op2[-1].split(";")
+                    ostatok = 0
+                    for op in obr:
+                        nom_op = op
+                        ostatok += self.summ_dost_det_po_nar(nom_mk, id_dse, nom_op)  # зеленый
+                    if ostatok <= 0:
+                        F.dob_color_wtab(tabl_mk, i - 1, j + 1, 0, 127, 0)  # зеленый
+
+    def max_det_skompl(self,nom_op,id_dse):
+        tabl_mk = self.ui.table_mk_view
+        for j in range(tabl_mk.rowCount()):
+            if tabl_mk.item(j,6).text() == id_dse:
+                for i in range(11,tabl_mk.columnCount(),4):
+                    if tabl_mk.item(j, i).text().strip() != '':
+                        obr = tabl_mk.item(j, i).text().strip().split('Операции:\n')
+                        obr2 = obr[-1].split(";")
+                        if str(nom_op) in obr2:
+                            if tabl_mk.item(j, i+1).text().strip() == '':
+                                return 0
+                            kompl = tabl_mk.item(j, i+1).text().strip().split(' шт.')
+                            return int(kompl[0])
 
 
+    def summ_dost_det_po_nar(self, nom_mar, id_dse, nom_op, zakr=False):
+        tabl_mk = self.ui.table_mk_view
+        sp_nar = F.otkr_f(F.tcfg('Naryad'), False, '|')
+        sp_zhur = F.otkr_f(F.tcfg('BDzhurnal'), False, '|')
+        if sp_nar == ['']:
+            showDialog(self, 'Не найдена база с нарядами')
+            return
+        max_det = self.max_det_skompl(nom_op, id_dse)
+        summ_det = 0
+        for i in range(len(sp_nar)):
+            if sp_nar[i][1] == nom_mar and sp_nar[i][25] == id_dse and sp_nar[i][24] == nom_op and sp_nar[i][21] == '':
+                if zakr == True:
+                    mn = []
+                    flag = 1
+                    for j in range(len(sp_zhur)):
+                        if sp_zhur[j][2] == sp_nar[i][0]:
+                            mn.append(sp_zhur[j][3])
+                    if len(mn) > 0:
+                        mn = set(mn)
+                        mn = list(mn)
+                        for j in range(len(sp_zhur)):
+                            if sp_zhur[j][2] == sp_nar[i][0] and sp_zhur[j][7] == 'Завершен':
+                                if sp_zhur[j][3] in mn:
+                                    mn.remove(sp_zhur[j][3])
+                            if len(mn) == 0:
+                                flag = 0
+                                break
+                        if flag == 0:
+                            summ_det += F.valm(sp_nar[i][12].strip())
+                else:
+                    summ_det += F.valm(sp_nar[i][12].strip())
+        if max_det - summ_det < 0:
+            return 0
+        return max_det - summ_det
 
     def oformlenie_sp_pod_mk(self,s):
         for j in s:
-            for i in range(10, len(s[0])):
+            for i in range(11, len(s[0]),4):
                 if '$' in j[i]:
                     vrem, oper1, oper2 = [x for x in j[i].split("$")]
                     j[i] = vrem + '\n' + oper1 + '\n' + oper2
+            for i in range(13, len(s[0]),4):
+                if '$' in j[i]:
+                    j[i] = j[i].replace('$','\n')
         return s
 
     def spisok_mk(self):
